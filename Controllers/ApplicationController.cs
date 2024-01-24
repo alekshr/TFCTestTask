cf3aa32d210f3fe2cd087aae285b2d1f74826f57
ashr <86759157+alekshr@users.noreply.github.com>
2024-01-24 07:14:59 +0700

    Создал вывод списка приложении

---
+++ Controllers/ApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TFCTestTask.Contexts;
+using TFCTestTask.Models;
+
+namespace TFCTestTask.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApplicationController : ControllerBase
+{
+    private readonly ApplicationContext applicationContext;
+
+    public ApplicationController(ApplicationContext applicationContext)
+    {
+        this.applicationContext = applicationContext;
+    }
+
+    [HttpGet]
+    public IEnumerable<Application> Get()
+    {
+        var applications = applicationContext.Applications.ToList();
+        return applications;
+    }
+}
